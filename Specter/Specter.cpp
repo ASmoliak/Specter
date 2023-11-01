7086a31e5ae5c412eb743fd6cb47c9750267789c
Alex Smoliak <alexs@cynet.com>
2023-11-01 18:14:03 +0200

    Set up basic MVP for communication

--- Specter/Specter.cpp
+++ Specter/Specter.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+#include <boost/beast.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 
 #include "UserInfo.hpp"
 #include "SystemInfo.hpp"
+
+#include <iostream>
 
 int main()
 {
@@ -12,7 +17,48 @@ int main()
 
 		// System info
 		std::cout << "Computer name: " << UserInfo::GetQualifiedUsername() << std::endl;
-		std::cout << "System uptime: " << SystemInfo::GetSystemUptime() << std::endl;
+		std::cout << "System uptime: " << SystemInfo::GetSystemUptime() << std::endl;	
+	
+		namespace http = boost::beast::http;
+
+		// Create I/O context, resolver, and socket
+		boost::asio::io_context ioc;
+		boost::asio::ip::tcp::resolver resolver(ioc);
+		boost::asio::ip::tcp::socket socket(ioc);
+
+		// Define target
+		auto const host = "localhost";
+		auto const port = "8001";
+		auto const target = "/registration/";
+
+		// Resolve and connect
+		boost::asio::connect(socket, resolver.resolve(host, port));
+
+		// Setup request
+		http::request<http::string_body> req(http::verb::get, target, 11);
+		req.set(http::field::host, host);
+		req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+		// Write, read and buffer the request
+		http::write(socket, req);
+		boost::beast::flat_buffer buffer;
+		http::response<http::dynamic_body> res;
+		http::read(socket, buffer, res);
+
+		// Print response
+		std::cout << res << std::endl;
+
+		// Cleanup (optional, handled automatically on object destruction)
+		boost::beast::error_code ec;
+		socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+
+		// Check and handle any errors
+		if (ec && ec != boost::beast::errc::not_connected)
+		{
+			throw boost::beast::system_error{ ec };
+		}
+			
+		return 0;
 	}
 	catch (const std::exception& e)
 	{
---
+++ SpecterServerMvp/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+
+string[] prefixes = new string[] { "http://localhost:8001/registration/" };
+
+SimpleListenerExample(prefixes);
+
+static void PrintHeaderIfExists(HttpListenerRequest request, string headerKey)
+{
+    if (request.Headers.AllKeys.Contains(headerKey))
+    {
+        Console.WriteLine($"{headerKey}: {request.Headers[headerKey]}");
+    }
+}
+
+static void SimpleListenerExample(string[] prefixes)
+{
+    if (!HttpListener.IsSupported)
+    {
+        Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+        return;
+    }
+
+    // URI prefixes are required,
+    // for example "http://127.0.0.1:8080/index/".
+    if (prefixes == null || prefixes.Length == 0)
+    {
+        throw new ArgumentException("prefixes");
+    }
+
+    // Create a listener.
+    HttpListener listener = new HttpListener();
+
+    // Add the prefixes.
+    foreach (string s in prefixes)
+    {
+        listener.Prefixes.Add(s);
+    }
+    listener.Start();
+    Console.WriteLine("Listening...");
+
+    // Note: The GetContext method blocks while waiting for a request.
+    while (true)
+    {
+        HttpListenerContext context = listener.GetContext();
+
+        HttpListenerRequest request = context.Request;
+        Console.WriteLine($"Received reg request from address: {request.UserHostAddress}");
+
+        if (request.Headers.Count > 0) 
+        {
+            PrintHeaderIfExists(request, "hdserial");
+            PrintHeaderIfExists(request, "username");
+            PrintHeaderIfExists(request, "computername");
+        }
+        
+        HttpListenerResponse response = context.Response;
+
+        // Construct a response.
+        string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+        // Get a response stream and write the response to it.
+        response.ContentLength64 = buffer.Length;
+        System.IO.Stream output = response.OutputStream;
+        output.Write(buffer, 0, buffer.Length);
+        // You must close the output stream.
+        output.Close();
+    }
+
+    //listener.Stop();
+}
