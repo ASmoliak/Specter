c5523682ae93827eb2ccc38f615d1dadc1877e2a
Alex Smoliak <alexs@cynet.com>
2023-11-06 00:41:56 +0200

    moved geolocator tests into a separated folder

--- SpecterServerTests/Tests/GeolocatorTests.cs
+++ SpecterServerTests/Tests/GeolocatorTests.cs
@@ -1,14 +1,14 @@
 using SpecterServer.Source.Utils;
 using SpecterServerUnitTests.TestUtils;
 
-namespace SpecterServerUnitTests
+namespace SpecterServerUnitTests.Tests
 {
     public class GeolocatorTests
     {
         [Fact]
         public void CanDownloadFile()
         {
-            TemporaryFile file = new(Geolocator.DbZipFileName);
+            TemporaryFile unused = new(Geolocator.DbZipFileName);
 
             Geolocator geolocator = new();
             GeolocatorDetectsGoogle(geolocator);
@@ -17,7 +17,7 @@ namespace SpecterServerUnitTests
         [Fact]
         public void CanUpdateFile()
         {
-            TemporaryFile file = new(Geolocator.DbZipFileName);
+            TemporaryFile unused = new(Geolocator.DbZipFileName);
 
             Geolocator geolocatorOriginal = new();
             Geolocator geolocator = new();
