0ddd16a2289f279ade468d72741834604658d8c0
Alex Smoliak <alexs@cynet.com>
2023-11-16 12:59:39 +0200

    Installer QoL Improvements

--- SpecterServer/Source/EndpointManager.cs
+++ SpecterServer/Source/EndpointManager.cs
@@ -14,7 +14,7 @@ namespace SpecterServer.Source
         public void AddOrUpdateEndpoint(HttpListenerRequest request)
         {
             var shortCountry = m_geolocator.IpToShortCountry(request.UserHostAddress);
-            var longCountry = m_geolocator.IpToLongCountry("8.8.8.8");
+            var longCountry = m_geolocator.IpToLongCountry(request.UserHostAddress);
 
             EndpointInfo args = new(request.Headers["hdserial"]!, request.UserHostAddress,
                                     request.Headers["osname"], request.Headers["machinename"],
