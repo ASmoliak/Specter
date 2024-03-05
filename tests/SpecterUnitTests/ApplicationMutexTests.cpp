2db626736f0348327de5d405c7c1f9cb2b00b7a4
Alex Smoliak <alexsmoliak@gmail.com>
2024-03-05 21:59:18 +0200

    Added unit tests targeting the ApplicationMutex

---
+++ tests/SpecterUnitTests/ApplicationMutexTests.cpp
@@ -0,0 +1,18 @@
+#include "SpecterLib/ApplicationMutex.h"
+
+TEST(ApplicationMutexTests, GetsOwnershipOfUniqueMutex)
+{
+	static const std::string sample_guid = "e5f3a187-e3a9-48f0-b6c5-457a2b46c08d";
+	const ApplicationMutex app_mutex(sample_guid);
+
+	EXPECT_TRUE(app_mutex.HasOwnership());
+}
+
+TEST(ApplicationMutexTests, FailsToOwnDuplicateMutex)
+{
+	static const std::string sample_guid = "7c4f6774-894b-47c3-8603-8714ba59374e";
+	const ApplicationMutex app_mutex(sample_guid);
+	const ApplicationMutex duplicate_app_mutex(sample_guid);
+
+	EXPECT_FALSE(duplicate_app_mutex.HasOwnership());
+}
